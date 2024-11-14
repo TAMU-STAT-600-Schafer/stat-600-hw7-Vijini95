91505faa40bb624238ba4efe5b9f2f98b2675dbc
Vijini <Vijinil@tamu.edu>
2024-11-13 21:07:55 -0600

    Check that the first column of X is 1s

--- FunctionsLR.R
+++ FunctionsLR.R
@@ -20,7 +20,10 @@ LRMultiClass <- function(X, y, Xt, yt, numIter = 50, eta = 0.1, lambda = 1, beta
   ## Check the supplied parameters as described. You can assume that X, Xt are matrices; y, yt are vectors; and numIter, eta, lambda are scalars. You can assume that beta_init is either NULL (default) or a matrix.
   ###################################
   # Check that the first column of X and Xt are 1s, if not - display appropriate message and stop execution.
-  
+  # Check that the first column of X is 1s, if not - display appropriate message and stop execution.
+  if (any(X[, 1] != 1)) {
+    stop("Error: check that the first column of X is 1s.")
+  }
   # Check for compatibility of dimensions between X and Y
   
   # Check for compatibility of dimensions between Xt and Yt
