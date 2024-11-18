a53f6807db2bd524c25183b405ddd4746522f644
Vijini <Vijinil@tamu.edu>
2024-11-17 19:13:44 -0600

    Reformat the code

--- Test_letter_NN.R
+++ Test_letter_NN.R
@@ -92,66 +92,287 @@ timings
 # [ToDo] Try changing the parameters above to obtain a better performance,
 # this will likely take several trials
 #set lambda = 0.01
-out3 = NN_train(Xtrain, Ytrain, Xval, Yval, lambda = 0.01, rate = 0.1, mbatch = 50, nEpoch = 150, hidden_p = 100, scale = 1e-3, seed = 12345)
-test_error1 = evaluate_error(Xt, Yt, out3$params$W1, out3$params$b1, out3$params$W2, out3$params$b2)
+out3 = NN_train(
+  Xtrain,
+  Ytrain,
+  Xval,
+  Yval,
+  lambda = 0.01,
+  rate = 0.1,
+  mbatch = 50,
+  nEpoch = 150,
+  hidden_p = 100,
+  scale = 1e-3,
+  seed = 12345
+)
+test_error1 = evaluate_error(Xt,
+                             Yt,
+                             out3$params$W1,
+                             out3$params$b1,
+                             out3$params$W2,
+                             out3$params$b2)
 test_error1 #28.45
 
 #set lambda = 0.0001
-out4 = NN_train(Xtrain, Ytrain, Xval, Yval, lambda = 0.0001, rate = 0.1, mbatch = 50, nEpoch = 150, hidden_p = 100, scale = 1e-3, seed = 12345)
-test_error2 = evaluate_error(Xt, Yt, out4$params$W1, out4$params$b1, out4$params$W2, out4$params$b2)
+out4 = NN_train(
+  Xtrain,
+  Ytrain,
+  Xval,
+  Yval,
+  lambda = 0.0001,
+  rate = 0.1,
+  mbatch = 50,
+  nEpoch = 150,
+  hidden_p = 100,
+  scale = 1e-3,
+  seed = 12345
+)
+test_error2 = evaluate_error(Xt,
+                             Yt,
+                             out4$params$W1,
+                             out4$params$b1,
+                             out4$params$W2,
+                             out4$params$b2)
 test_error2 #14.56667 (when lambda is small, error is small)
 
 #set rate = 0.01, lambda = 0.0001
-out5 = NN_train(Xtrain, Ytrain, Xval, Yval, lambda = 0.0001, rate = 0.01, mbatch = 50, nEpoch = 150, hidden_p = 100, scale = 1e-3, seed = 12345)
-test_error3 = evaluate_error(Xt, Yt, out5$params$W1, out5$params$b1, out5$params$W2, out5$params$b2)
+out5 = NN_train(
+  Xtrain,
+  Ytrain,
+  Xval,
+  Yval,
+  lambda = 0.0001,
+  rate = 0.01,
+  mbatch = 50,
+  nEpoch = 150,
+  hidden_p = 100,
+  scale = 1e-3,
+  seed = 12345
+)
+test_error3 = evaluate_error(Xt,
+                             Yt,
+                             out5$params$W1,
+                             out5$params$b1,
+                             out5$params$W2,
+                             out5$params$b2)
 test_error3 #27.44444
 
 #set rate = 0.5, lambda = 0.001
-out6 = NN_train(Xtrain, Ytrain, Xval, Yval, lambda = 0.001, rate = 0.5, mbatch = 50, nEpoch = 150, hidden_p = 100, scale = 1e-3, seed = 12345)
-test_error4 = evaluate_error(Xt, Yt, out6$params$W1, out6$params$b1, out6$params$W2, out6$params$b2)
-test_error4 #96.37778 
+out6 = NN_train(
+  Xtrain,
+  Ytrain,
+  Xval,
+  Yval,
+  lambda = 0.001,
+  rate = 0.5,
+  mbatch = 50,
+  nEpoch = 150,
+  hidden_p = 100,
+  scale = 1e-3,
+  seed = 12345
+)
+test_error4 = evaluate_error(Xt,
+                             Yt,
+                             out6$params$W1,
+                             out6$params$b1,
+                             out6$params$W2,
+                             out6$params$b2)
+test_error4 #96.37778
 
 #set rate = 0.11, lambda = 0.001
-out7 = NN_train(Xtrain, Ytrain, Xval, Yval, lambda = 0.001, rate = 0.11, mbatch = 50, nEpoch = 150, hidden_p = 100, scale = 1e-3, seed = 12345)
-test_error5 = evaluate_error(Xt, Yt, out7$params$W1, out7$params$b1, out7$params$W2, out7$params$b2)
+out7 = NN_train(
+  Xtrain,
+  Ytrain,
+  Xval,
+  Yval,
+  lambda = 0.001,
+  rate = 0.11,
+  mbatch = 50,
+  nEpoch = 150,
+  hidden_p = 100,
+  scale = 1e-3,
+  seed = 12345
+)
+test_error5 = evaluate_error(Xt,
+                             Yt,
+                             out7$params$W1,
+                             out7$params$b1,
+                             out7$params$W2,
+                             out7$params$b2)
 test_error5 #17.11667 (If rate is different from 0.1, then error is getting large)
 
 #set mbatch = 100
-out8 = NN_train(Xtrain, Ytrain, Xval, Yval, lambda = 0.001, rate = 0.1, mbatch = 100, nEpoch = 150, hidden_p = 100, scale = 1e-3, seed = 12345)
-test_error6 = evaluate_error(Xt, Yt, out8$params$W1, out8$params$b1, out8$params$W2, out8$params$b2)
-test_error6 #18.70556 
+out8 = NN_train(
+  Xtrain,
+  Ytrain,
+  Xval,
+  Yval,
+  lambda = 0.001,
+  rate = 0.1,
+  mbatch = 100,
+  nEpoch = 150,
+  hidden_p = 100,
+  scale = 1e-3,
+  seed = 12345
+)
+test_error6 = evaluate_error(Xt,
+                             Yt,
+                             out8$params$W1,
+                             out8$params$b1,
+                             out8$params$W2,
+                             out8$params$b2)
+test_error6 #18.70556
 
 #set mbatch = 45
-out9 = NN_train(Xtrain, Ytrain, Xval, Yval, lambda = 0.001, rate = 0.1, mbatch = 45, nEpoch = 150, hidden_p = 100, scale = 1e-3, seed = 12345)
-test_error7 = evaluate_error(Xt, Yt, out9$params$W1, out9$params$b1, out9$params$W2, out9$params$b2)
+out9 = NN_train(
+  Xtrain,
+  Ytrain,
+  Xval,
+  Yval,
+  lambda = 0.001,
+  rate = 0.1,
+  mbatch = 45,
+  nEpoch = 150,
+  hidden_p = 100,
+  scale = 1e-3,
+  seed = 12345
+)
+test_error7 = evaluate_error(Xt,
+                             Yt,
+                             out9$params$W1,
+                             out9$params$b1,
+                             out9$params$W2,
+                             out9$params$b2)
 test_error7 #18.91111 (If mbatch is different from 50, then error is getting large)
 
 #set nEpoch = 300
-out10 = NN_train(Xtrain, Ytrain, Xval, Yval, lambda = 0.001, rate = 0.1, mbatch = 50, nEpoch = 300, hidden_p = 100, scale = 1e-3, seed = 12345)
-test_error8 = evaluate_error(Xt, Yt, out10$params$W1, out10$params$b1, out10$params$W2, out10$params$b2)
+out10 = NN_train(
+  Xtrain,
+  Ytrain,
+  Xval,
+  Yval,
+  lambda = 0.001,
+  rate = 0.1,
+  mbatch = 50,
+  nEpoch = 300,
+  hidden_p = 100,
+  scale = 1e-3,
+  seed = 12345
+)
+test_error8 = evaluate_error(Xt,
+                             Yt,
+                             out10$params$W1,
+                             out10$params$b1,
+                             out10$params$W2,
+                             out10$params$b2)
 test_error8 #29.44444 (If mbatch is different from 50, then error is getting large)
 
 #set nEpoch = 100
-out11 = NN_train(Xtrain, Ytrain, Xval, Yval, lambda = 0.001, rate = 0.1, mbatch = 50, nEpoch = 100, hidden_p = 100, scale = 1e-3, seed = 12345)
-test_error9 = evaluate_error(Xt, Yt, out11$params$W1, out11$params$b1, out11$params$W2, out11$params$b2)
+out11 = NN_train(
+  Xtrain,
+  Ytrain,
+  Xval,
+  Yval,
+  lambda = 0.001,
+  rate = 0.1,
+  mbatch = 50,
+  nEpoch = 100,
+  hidden_p = 100,
+  scale = 1e-3,
+  seed = 12345
+)
+test_error9 = evaluate_error(Xt,
+                             Yt,
+                             out11$params$W1,
+                             out11$params$b1,
+                             out11$params$W2,
+                             out11$params$b2)
 test_error9 #16.85556 (If mbatch is different from 150, then error is getting large)
 
-#set hidden_p = 200 
-out12 = NN_train(Xtrain, Ytrain, Xval, Yval, lambda = 0.001, rate = 0.1, mbatch = 50, nEpoch = 150, hidden_p = 200, scale = 1e-3, seed = 12345)
-test_error10 = evaluate_error(Xt, Yt, out12$params$W1, out12$params$b1, out12$params$W2, out12$params$b2)
+#set hidden_p = 200
+out12 = NN_train(
+  Xtrain,
+  Ytrain,
+  Xval,
+  Yval,
+  lambda = 0.001,
+  rate = 0.1,
+  mbatch = 50,
+  nEpoch = 150,
+  hidden_p = 200,
+  scale = 1e-3,
+  seed = 12345
+)
+test_error10 = evaluate_error(Xt,
+                              Yt,
+                              out12$params$W1,
+                              out12$params$b1,
+                              out12$params$W2,
+                              out12$params$b2)
 test_error10 #15.13889 (If mbatch is different from 150, then error is getting large)
 
-#set hidden_p = 1000 
-out13 = NN_train(Xtrain, Ytrain, Xval, Yval, lambda = 0.001, rate = 0.1, mbatch = 50, nEpoch = 150, hidden_p = 1000, scale = 1e-3, seed = 12345)
-test_error11 = evaluate_error(Xt, Yt, out13$params$W1, out13$params$b1, out13$params$W2, out13$params$b2)
+#set hidden_p = 1000
+out13 = NN_train(
+  Xtrain,
+  Ytrain,
+  Xval,
+  Yval,
+  lambda = 0.001,
+  rate = 0.1,
+  mbatch = 50,
+  nEpoch = 150,
+  hidden_p = 1000,
+  scale = 1e-3,
+  seed = 12345
+)
+test_error11 = evaluate_error(Xt,
+                              Yt,
+                              out13$params$W1,
+                              out13$params$b1,
+                              out13$params$W2,
+                              out13$params$b2)
 test_error11 #13.44444 (If hidden_p is increasing, then error is getting small)
 
 #set scale = 1e-2
-out14 = NN_train(Xtrain, Ytrain, Xval, Yval, lambda = 0.001, rate = 0.1, mbatch = 50, nEpoch = 150, hidden_p = 150, scale = 1e-2, seed = 12345)
-test_error12 = evaluate_error(Xt, Yt, out14$params$W1, out14$params$b1, out14$params$W2, out14$params$b2)
-test_error12 #15.06111 
+out14 = NN_train(
+  Xtrain,
+  Ytrain,
+  Xval,
+  Yval,
+  lambda = 0.001,
+  rate = 0.1,
+  mbatch = 50,
+  nEpoch = 150,
+  hidden_p = 150,
+  scale = 1e-2,
+  seed = 12345
+)
+test_error12 = evaluate_error(Xt,
+                              Yt,
+                              out14$params$W1,
+                              out14$params$b1,
+                              out14$params$W2,
+                              out14$params$b2)
+test_error12 #15.06111
 
 #set scale = 1e-5
-out15 = NN_train(Xtrain, Ytrain, Xval, Yval, lambda = 0.001, rate = 0.1, mbatch = 50, nEpoch = 150, hidden_p = 150, scale = 1e-5, seed = 12345)
-test_error13 = evaluate_error(Xt, Yt, out15$params$W1, out15$params$b1, out15$params$W2, out15$params$b2)
+out15 = NN_train(
+  Xtrain,
+  Ytrain,
+  Xval,
+  Yval,
+  lambda = 0.001,
+  rate = 0.1,
+  mbatch = 50,
+  nEpoch = 150,
+  hidden_p = 150,
+  scale = 1e-5,
+  seed = 12345
+)
+test_error13 = evaluate_error(Xt,
+                              Yt,
+                              out15$params$W1,
+                              out15$params$b1,
+                              out15$params$W2,
+                              out15$params$b2)
 test_error13 #16.48889 (If scale is increasing, then error is getting small)
